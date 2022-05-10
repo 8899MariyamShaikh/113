a38783f348ad72a6e674784efb63b2f81e1ce857
Mariyam_op <91929003+8899MariyamShaikh@users.noreply.github.com>
2022-05-10 17:07:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,88 +1,36 @@
-prediction_1 = ""
-prediction_2 = ""
+function preload()
+{}
 
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90 
-});
+function setup()
+{
+    canvas = createCanvas ( 640, 450);
+    canvas.position( 150, 150 );
+    video = createCapture(VIDEO);
+    video.hide();
+}
 
-camera = document.getElementById("camera");
+function draw()
+{
+    image(video, 250, 150, 220, 200)
 
-Webcam.attach('#camera')
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(590, 430, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+
+    rect(90, 420, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(580, 90, 20, 300);
+}
 
 function take_snapshot()
 {
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
-    });
-}
-
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
-
-function modelLoaded()
-{
-    console.log('ModelLoaded');
-}
-
-function speak()
-{
-    var synth = window.speechSynthesis;
-    speak_data1 = "The First Prediction is "+prediction_1;
-    speak_data2 = "The Second Prediction is "+prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
-    synth.speak(utterThis);
-}
-
-function check()
-{
-    img = document.getElementById('capture_image');
-    classifier.classify(img.gotResult);
-}
-
-function gotResult(error, results)
-{
-    if (error) {
-        console.error(error);
-    }
-    else {
-        console.log(results);
-        document.getElementById("result_emotion_name").innerHTML = results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML =  results[1].label;
-        prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
-
-        speak();
-        if(results[0].label == "happy")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128522;";
-        }
-
-        if(results[0].label == "sad")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128532;";
-        }
-
-        if(results[0].label == "angry")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128548;";
-        }
-
-        if(results[1].label == "happy")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128522;";
-        }
-
-        if(results[1].label == "sad")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128532;";
-        }
-
-        if(results[1].label == "angry")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128548;";
-        }
-    }
+    save('Border_Filter_image.png');
 }
